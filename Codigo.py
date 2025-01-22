e80268e1277924312e68d63f5bc4ae80d532725a
Daniel1205PP <dpalaciospo@unal.edu.co>
2025-01-22 16:55:40 +0000

    avan

--- Codigo.py
+++ Codigo.py
@@ -12,7 +12,24 @@ def registrar_usuario():
 
     print("\nUsuario registrado con éxito.")
 
-# Ejecutar la función de registro
+def elegir_modo_juego():
+    print("\n=== Selección de Modo de Juego ===")
+    print("1. Individual")
+    print("2. Dos Jugadores")
+
+    while True:
+        opcion = input("Elige una opción (1 o 2): ")
+        if opcion == "1":
+            print("Has elegido el modo de juego individual.")
+            break
+        elif opcion == "2":
+            print("Has elegido el modo de juego para dos jugadores.")
+            break
+        else:
+            print("Opción no válida. Por favor, elige 1 o 2.")
+
+# Ejecutar las funciones
 registrar_usuario()
+elegir_modo_juego()
 
 print("Los usuarios registrados se guardan en el archivo 'usuarios.txt'.")
